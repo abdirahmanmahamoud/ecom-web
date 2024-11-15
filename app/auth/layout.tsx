3498ca1b4219fcf03b6211b2d4756dc68701b9a9
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2024-11-15 07:47:47 +0300

    add user login not auth red

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,13 @@
 import { Card } from "@/components/ui/card";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token");
+  if (token) {
+    return redirect("/");
+  }
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-slate-100">
       <Card className="md:w-3/4 lg:w-1/3 bg-white shadow-lg">{children}</Card>
